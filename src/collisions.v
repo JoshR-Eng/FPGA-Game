348522e5099900d03c761591e55649fd403ebdcf
JoshR-Eng <joshrawlinson05@outlook.com>
2026-04-12 17:33:08 +0100

    init asteroids & collision

---
+++ src/collisions.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.04.2026 16:25:32 
+// Design Name: 
+// Module Name: collisions 
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module game_top(
+  input clk,
+  input rst,
+  input frame_tick,
+
+  input [175:0] bul_x_packed,
+  input [175:0] bul_y_packed,
+  input [15:0] bul_active_packed,
+  );
+
+endmodule
