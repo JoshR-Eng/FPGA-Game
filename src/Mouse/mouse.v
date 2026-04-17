7276a51b4e9602c2233947695e31a10bd6793e9b
JoshR-Eng <joshrawlinson05@outlook.com>
2026-04-17 18:21:41 +0100

    fix mouse hdl

--- src/Mouse/mouse.v
+++ src/Mouse/mouse.v
@@ -35,27 +35,24 @@ module mouse(
     );
     
     // --- Internal registers --------------------------------------
-    wire [10:0]  word1, word2, word3, word4;
-    wire [7:0]   signal1, signal2, signal3, signal4;
-    wire         val;
-    
-    assign ready = val && ack;
+    wire [10:0]  word1, word2, word3;
+    wire [7:0]   signal1, signal2, signal3;
     
     
     // --- Map Signals ---------------------------------------------
     // Note: PS/2 sends LSB first, so we reverse the bits when extracting
-    assign x_pos = {signal2[0], signal2[1], signal2[2], signal2[3], signal2[4], signal2[5], signal2[6], signal2[7]};
-    assign y_pos = {signal3[0], signal3[1], signal3[2], signal3[3], signal3[4], signal3[5], signal3[6], signal3[7]};
-    assign x_sign = signal1[3];  // Bit 4 of status byte
-    assign y_sign = signal1[2];  // Bit 5 of status byte
-    assign left_btn = signal1[7]; // Bit 0 of status byte
-    assign right_btn = signal1[6]; // Bit 1 of status byte
+    assign x_pos      = signal2;
+    assign y_pos      = signal3;
+    assign x_sign     = signal1[3];  // Bit 4 of status byte
+    assign y_sign     = signal1[2];  // Bit 5 of status byte
+    assign left_btn   = signal1[7]; // Bit 0 of status byte
+    assign right_btn  = signal1[6]; // Bit 1 of status byte
     assign middle_btn = signal1[5]; // Bit 2 of status byte 
     
     
     // --- Signal Processing ---------------------------------------
-    reg [43:0]  fifo;
-    reg [43:0]  buffer;
+    reg [32:0]  fifo;
+    reg [32:0]  buffer;
     reg [5:0]   counter;
     reg [1:0]   PS2Clk_sync;
     reg         PS2Data;
@@ -65,23 +62,13 @@ module mouse(
     assign word1 = fifo[33 +: 11];
     assign word2 = fifo[22 +: 11];
     assign word3 = fifo[11 +: 11];
-    assign word4 = fifo[0 +: 11];
     
     assign PS2Clk_negedge = (PS2Clk_sync == 2'b10);
     
-    initial begin
-        fifo <= 44'b0;
-        buffer <= 44'b0;
-        counter <= 6'b0;
-        PS2Clk_sync <= 2'b1;
-        ack <= 1'b0;
-        PS2Data <= 1'b0;
-    end
-    
     always @(posedge clk) begin 
-        if (rst) begin
-            fifo <= 44'b0;
-            buffer <= 44'b0;
+        if (!rst) begin
+            fifo <= 33'b0;
+            buffer <= 33'b0;
             counter <= 6'b0;
             ack <= 1'b0;
             PS2Clk_sync <= 2'b11;
@@ -92,14 +79,14 @@ module mouse(
             PS2Data <= mouse_data;
             
             if (PS2Clk_negedge) begin
-                buffer <= {buffer[42:0], PS2Data};
+                buffer <= {buffer[31:0], PS2Data};
                 counter <= counter + 6'b1;
             end
             
-            if (counter == 6'd44) begin
+            if (counter == 6'd33) begin
                 // Counter == 44 --> Buffer is FULL
                 fifo <= buffer;
-                buffer <= 44'b0;
+                buffer <= 33'b0;
                 counter <= 6'b0;
                 ack <= 1'b1;
             end else begin
@@ -111,26 +98,24 @@ module mouse(
 
 
     // --- Signal Validation ---------------------------------------
-    wire parity1, parity2, parity3, parity4, parity;
-    wire start1, start2, start3, start4, start;
-    wire stop1, stop2, stop3, stop4, stop;
-    wire valid1, valid2, valid3, valid4;
+    wire parity1, parity2, parity3, parity;
+    wire start1, start2, start3, start;
+    wire stop1, stop2, stop3, stop;
+    wire valid1, valid2, valid3;
 
     // Separate packets into segments
     assign {start1, signal1, parity1, stop1} = word1;
     assign {start2, signal2, parity2, stop2} = word2;
     assign {start3, signal3, parity3, stop3} = word3;
-    assign {start4, signal4, parity4, stop4} = word4;
     
     // XNOR words together and compare to parity bit
     assign valid1 = ~^signal1 == parity1;
     assign valid2 = ~^signal2 == parity2;
     assign valid3 = ~^signal3 == parity3;
-    assign valid4 = ~^signal4 == parity4;
     
-    assign parity = valid1 && valid2 && valid3 && valid4;
-    assign start = (!start1 && !start2 && !start3 && !start4);
-    assign stop = (stop1 && stop2 && stop3 && stop4);
+    assign parity = valid1 && valid2 && valid3;
+    assign start = (!start1 && !start2 && !start3);
+    assign stop = (stop1 && stop2 && stop3);
     assign o_valid = (start && stop && parity);
 
 endmodule
