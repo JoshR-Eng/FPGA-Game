77a336af01daed7ab01f7c0413c7d48a044fd1c9
JoshR-Eng <joshrawlinson05@outlook.com>
2026-04-17 12:00:41 +0100

    update seven seg to display logic

--- src/sevenseg/scoreDisplay.v
+++ src/sevenseg/scoreDisplay.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 02.02.2026 14:31:41
 // Design Name: 
-// Module Name: seginterface
+// Module Name: scoreDisplay 
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,24 +20,36 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module seginterface(
-    input clk, rst,
-    input [3:0] dig7, dig6, dig5, dig4, dig3, dig2, dig1, dig0,
-    output div_clk,
+module scoreDisplay(
+    // System
+    input clk, 
+    input rst,
+
+    // Display Value
+    input [16:0] score, 
+    input [1:0]  health,
+
+    // Seven-seg Pin
     output a, b, c, d, e, f, g,
     output [7:0] an 
     );
     
+// ==========================================================
+// --- Internal Wiring
+// ==========================================================
     wire led_clk;
     
     reg [3:0] dig_sel;
-    reg [28:0] clk_count = 11'd0;
+    reg [16:0] clk_count = 11'd0;
     
+// ==========================================================
+// --- Ring Counter 
+// ==========================================================
+
     // Make a counter that uses the clock
     always @(posedge clk) 
         clk_count <= clk_count + 1'b1;
     assign led_clk = clk_count[16]; // 2^16 slower than clk_count
-    assign div_clk = clk_count[21]; // 2^25 slower than clk_count
     
     // Ring Counter
     //  Since 7-seg is active low, 0 turns the digit on
@@ -54,17 +66,31 @@ module seginterface(
     led_index <= led_index + 1'b1;
     
     
+// ==========================================================
+// --- Binary -> Decimal Mapping 
+// ==========================================================
+
+wire [3:0] thousands  = score           /1000;
+wire [3:0] hundreds   = (score % 1000)  /100;
+wire [3:0] tens       = (score % 100 )  /10;
+wire [3:0] units      = (score % 10);
+
+
+// ==========================================================
+// --- Seven-Seg Assignment 
+// ==========================================================
+
     // 
     always@*    
         case (led_index)
-            3'd0: dig_sel = dig0;
-            3'd1: dig_sel = dig1;
-            3'd2: dig_sel = dig2;
-            3'd3: dig_sel = dig3;
-            3'd4: dig_sel = dig4;
-            3'd5: dig_sel = dig5;
-            3'd6: dig_sel = dig6;
-            3'd7: dig_sel = dig7;
+            3'd0: dig_sel = units;
+            3'd1: dig_sel = tens;
+            3'd2: dig_sel = hundreds;
+            3'd3: dig_sel = thousands;
+            3'd4: dig_sel = 4'b0;
+            3'd5: dig_sel = 4'b0;
+            3'd6: dig_sel = 4'b0;
+            3'd7: dig_sel = { 2'b0, health};
         endcase        
     
     sevenseg inst (.num(dig_sel), .a(a), .b(b), .c(c), .d(d), .e(e), .f(f), .g(g));
